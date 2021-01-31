156399544ab0ebb2aca30b9dcdfe7bb4453bb54d
Melek Karaca <melekkaraca@gmail.com>
2021-01-31 16:04:40 +0300

    Add project files.

---
+++ ConsoleIU/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleIU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine(item.ModelYear + " " + item.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,50 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> { 
+                new Car {Id = 1, BrandId = 1, ColorId = 1, ModelYear = 2020, DailyPrice = 225, Description = "Açıklama 1"},
+                new Car {Id = 2, BrandId = 1, ColorId = 1, ModelYear = 2021, DailyPrice = 450, Description = "Açıklama 2"},
+                new Car {Id = 3, BrandId = 2, ColorId = 3, ModelYear = 2018, DailyPrice = 170, Description = "Açıklama 3"},
+                new Car {Id = 4, BrandId = 3, ColorId = 2, ModelYear = 2019, DailyPrice = 169, Description = "Açıklama 4"},
+                new Car {Id = 5, BrandId = 3, ColorId = 3, ModelYear = 2019, DailyPrice = 169, Description = "Açıklama 5"},
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+        public Car GetById(int id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == id);
+        }
+    }
+}
