243e2a15b1ca047316303f97b2ea0c84342719d0
Melek Karaca <melekkaraca@gmail.com>
2021-03-02 23:18:07 +0300

    tamamlandı

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Business;
+using Core.Entities.Concrete;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,5 +9,7 @@ namespace Business.Abstract
 {
     public interface IUserService : IBusinessRepository<User>
     {
+        List<OperationClaim> GetClaims(User user);
+        User GetByMail(string email);
     }
 }
